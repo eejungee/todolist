e1a3b037e5c1bdc66187ba3615f371b69186d94d
eejungee <sulau861@gmail.com>
2023-04-13 18:22:42 +0900

    FIX: 댓글 수정, 삭제 오류 해결

--- src/pages/ToDo/TodoComponents/TodoList/TodoList.jsx
+++ src/pages/ToDo/TodoComponents/TodoList/TodoList.jsx
@@ -2,9 +2,16 @@ import React, { useState } from "react";
 import API from "../../../../config/config";
 import * as S from "./TodoList.style";
 
+<<<<<<< HEAD
 const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
   const [isUpdateClicked, setIsUpdateClicked] = useState(false);
   const [updateInput, setUpdateInput] = useState(todo.todo);
+=======
+const TodoList = ({ todo, index, setTodos, todoInput, handleTodoInput }) => {
+  const [isUpdateClicked, setIsUpdateClicked] = useState(false);
+  const [updateInput, setUpdateInput] = useState(todo.todo);
+  const [isDeleted, setIsDeleted] = useState(false);
+>>>>>>> 8f02702 (ADD: todo 기능 및 디자인)
 
   const token = localStorage.getItem("token");
 
@@ -15,6 +22,7 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
 
   const handleUpdateInput = (e) => setUpdateInput(e.target.value);
 
+<<<<<<< HEAD
   const updateCheckBox = (id, index, e) => {
     setTodos((prev) => {
       const updatedTodos = [...prev];
@@ -23,6 +31,40 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
     });
 
     fetch(`${API.TODO}/${id}`, {
+=======
+  // 댓글 삭제
+  const deleteTodo = (id) => {
+    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status === 204) {
+          setIsDeleted(true);
+          alert("댓글이 삭제됐어요!");
+        } else {
+          alert("댓글을 삭제할 수 없어요!");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(`Error : ${error}`);
+      });
+  };
+
+  // 체크 수정
+  const updateCheckBox = (id, index, e) => {
+    setTodos((prev) => {
+      const updatedTodos = [...prev];
+      updatedTodos[index].isCompleted = e.target.checked;
+      return updatedTodos;
+    });
+
+    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+>>>>>>> 8f02702 (ADD: todo 기능 및 디자인)
       method: "PUT",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -33,6 +75,7 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
         isCompleted: e.target.checked,
       }),
     })
+<<<<<<< HEAD
       .then((res) => {
         if (res.status === 200) {
           alert("완료 상태가 수정됐어요!");
@@ -40,10 +83,14 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
           alert("완료 상태를 수정할 수 없어요!");
         }
       })
+=======
+      .then((res) => res.json())
+>>>>>>> 8f02702 (ADD: todo 기능 및 디자인)
       .catch((error) => {
         console.error(error);
         alert(`Error : ${error}`);
       });
+    console.log("업데이트됐니?", todo);
   };
 
   const updateTodo = (id, index, updateInput, e) => {
@@ -82,7 +129,11 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
 
   return (
     <>
+<<<<<<< HEAD
       {todo && (
+=======
+      {todo && !isDeleted && (
+>>>>>>> 8f02702 (ADD: todo 기능 및 디자인)
         <S.TodoList>
           <input
             type="checkbox"
@@ -128,7 +179,11 @@ const TodoList = ({ todo, index, setTodos, deleteTodo }) => {
                 <S.RightBtn
                   type="button"
                   data-testid="delete-button"
+<<<<<<< HEAD
                   onClick={(e) => deleteTodo(todo.id, e)}
+=======
+                  onClick={() => deleteTodo(todo.id)}
+>>>>>>> 8f02702 (ADD: todo 기능 및 디자인)
                 >
                   삭제
                 </S.RightBtn>
